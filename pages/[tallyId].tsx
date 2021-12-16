f4b415e05d0cea04b7923131d33aa5b6f987dc7a
Martin Dennhardt <martin@dennhardt.de>
2021-12-16 15:34:20 +0000

    Add title

--- pages/[tallyId].tsx
+++ pages/[tallyId].tsx
@@ -6,8 +6,9 @@ import {
   mdiPlus,
   mdiRotateLeft,
 } from "@mdi/js";
-import { FC, useContext, useEffect, useRef, useState } from "react";
+import React, { FC, useContext, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import { useTally } from "../hooks/useTally";
 import TallyCounter from "../components/TallyCounter";
 import Button from "../components/Button";
@@ -52,89 +53,97 @@ const Tally: FC = () => {
   }, [showToast, dismissToast, onReconnected, onReconnecting]);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col items-center bg-white dark:bg-gray-800 shadow-xl ring-1 ring-gray-900/5 rounded-lg pb-12 pt-4 px-8 w-80">
-        {tallyId && !tallyId.match("^[a-zA-Z0-9]{1,16}$") ? (
-          <Error />
-        ) : (
-          <>
-            <TallyCounter
-              tally={tally?.count}
-              disabled={disabled}
-              onClick={increment}
-              className="text-emerald-500"
-            />
-            <div className="flex items-center">
-              <div className="flex relative">
-                <Button
-                  className={classNames(
-                    "transition bg-emerald-500 text-white dark:text-black rounded-full absolute m-1",
-                    {
-                      "-translate-x-24": advanced,
-                      "opacity-0": !advanced,
-                    }
-                  )}
-                  size="md"
-                  onClick={decrement}
-                  disabled={disabled}
-                >
-                  <Icon path={mdiMinus} size={1.5} />
-                </Button>
-                <Button
-                  className={classNames(
-                    "transition bg-emerald-500 text-white dark:text-black rounded-full absolute m-1",
-                    {
-                      "-translate-x-12": advanced,
-                      "opacity-0": !advanced,
-                    }
-                  )}
-                  size="md"
-                  onClick={reset}
-                  disabled={disabled}
-                >
-                  <Icon path={mdiRotateLeft} size={1.5} />
-                </Button>
-                <Button
-                  className="text-emerald-500 rounded-full z-10 m-1"
-                  size="flat"
-                  onClick={() => setAdvanced((advanced) => !advanced)}
-                >
-                  <Icon path={mdiDotsHorizontal} size={1.5} />
-                </Button>
-              </div>
-              <Button
-                className="bg-emerald-500 text-white dark:text-black rounded-full m-2"
-                size="lg"
-                onClick={() => {
-                  increment();
-                  setAdvanced(false);
-                }}
+    <>
+      <Head>
+        <title>
+          {tally && tally?.count + " | " + tally?.id + " | "}myTally - shared
+          tally counters
+        </title>
+      </Head>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col items-center bg-white dark:bg-gray-800 shadow-xl ring-1 ring-gray-900/5 rounded-lg pb-12 pt-4 px-8 w-80">
+          {tallyId && !tallyId.match("^[a-zA-Z0-9]{1,16}$") ? (
+            <Error />
+          ) : (
+            <>
+              <TallyCounter
+                tally={tally?.count}
                 disabled={disabled}
-              >
-                <Icon path={mdiPlus} size={3} />
-              </Button>
-              <CopyToClipboard
-                text={shareLink}
-                onCopy={() => {
-                  showToast({
-                    children:
-                      "I copied the link to this Tally to your clipboard. Go ahead and share it with your team.",
-                    duration: 5000,
-                  });
-                }}
-              >
+                onClick={increment}
+                className="text-emerald-500"
+              />
+              <div className="flex items-center">
+                <div className="flex relative">
+                  <Button
+                    className={classNames(
+                      "transition bg-emerald-500 text-white dark:text-black rounded-full absolute m-1",
+                      {
+                        "-translate-x-24": advanced,
+                        "opacity-0": !advanced,
+                      }
+                    )}
+                    size="md"
+                    onClick={decrement}
+                    disabled={disabled}
+                  >
+                    <Icon path={mdiMinus} size={1.5} />
+                  </Button>
+                  <Button
+                    className={classNames(
+                      "transition bg-emerald-500 text-white dark:text-black rounded-full absolute m-1",
+                      {
+                        "-translate-x-12": advanced,
+                        "opacity-0": !advanced,
+                      }
+                    )}
+                    size="md"
+                    onClick={reset}
+                    disabled={disabled}
+                  >
+                    <Icon path={mdiRotateLeft} size={1.5} />
+                  </Button>
+                  <Button
+                    className="text-emerald-500 rounded-full z-10 m-1"
+                    size="flat"
+                    onClick={() => setAdvanced((advanced) => !advanced)}
+                  >
+                    <Icon path={mdiDotsHorizontal} size={1.5} />
+                  </Button>
+                </div>
                 <Button
-                  className="rounded-full m-1 text-emerald-500"
-                  size="flat"
+                  className="bg-emerald-500 text-white dark:text-black rounded-full m-2"
+                  size="lg"
+                  onClick={() => {
+                    increment();
+                    setAdvanced(false);
+                  }}
+                  disabled={disabled}
                 >
-                  <Icon path={mdiExportVariant} size={1.5} />
+                  <Icon path={mdiPlus} size={3} />
                 </Button>
-              </CopyToClipboard>
-            </div>
-          </>
-        )}
+                <CopyToClipboard
+                  text={shareLink}
+                  onCopy={() => {
+                    showToast({
+                      children:
+                        "I copied the link to this Tally to your clipboard. Go ahead and share it with your team.",
+                      duration: 5000,
+                    });
+                  }}
+                >
+                  <Button
+                    className="rounded-full m-1 text-emerald-500"
+                    size="flat"
+                  >
+                    <Icon path={mdiExportVariant} size={1.5} />
+                  </Button>
+                </CopyToClipboard>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toasts } from "../components/Toasts";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Toasts>
+      <Head>
+        <title>myTally - shared tally counters</title>
+      </Head>
       <div className="text-black bg-gray-50 dark:text-white dark:bg-gray-900">
         <Component {...pageProps} />
       </div>
