d8e7422075392cbbb6ab2e2db8304666707700ff
Martin Dennhardt <martin@dennhardt.de>
2022-03-20 21:06:49 +0000

    add grid cols 1-4 to tailwind safelist
    dynamically set classes are purged otherwise

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    {
+      pattern: /grid-cols-(1|2|3|4)/,
+      variants: ["md", "lg", "xl"],
+    },
+  ],
   theme: {
     extend: {},
   },
