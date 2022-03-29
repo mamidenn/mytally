3a1db03bc5e196a3e2acf09be0fec2948a70f51d
Martin Dennhardt <martin@dennhardt.de>
2022-03-29 07:08:35 +0000

    introduce constants for valid tally id lengths

--- modules/tally.ts
+++ modules/tally.ts
@@ -2,7 +2,10 @@ import { randomInt } from "crypto";
 import { range } from "lodash";
 
 export const validateId = (id: string) =>
-  id.match("^[a-zA-Z0-9]{1,16}$") !== null;
+  id.match(`^[a-zA-Z0-9]{${minLength},${maxLength}}$`) !== null;
 
 export const getRandomId = () =>
   range(8).reduce((c, _) => (c += randomInt(36).toString(36)), "");
+
+export const minLength = 1;
+export const maxLength = 16;
