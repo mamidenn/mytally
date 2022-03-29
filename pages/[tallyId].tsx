16fd7b71b3297a3840245a09c4b92cff112735a5
Martin Dennhardt <martin@dennhardt.de>
2022-03-29 09:39:42 +0000

    enable toggling the "add tally" card

--- pages/[tallyId].tsx
+++ pages/[tallyId].tsx
@@ -16,6 +16,8 @@ import {
   validateId,
   validationMessage,
 } from "modules/tally";
+import Icon from "@mdi/react";
+import { mdiClose } from "@mdi/js";
 
 const TallyPage: FC = () => {
   const router = useRouter();
@@ -30,6 +32,7 @@ const TallyPage: FC = () => {
   const [addtlId, setAddtlId] = useState<string>("");
   const [isValid, setIsValid] = useState<boolean>();
   const [submitted, setSubmitted] = useState<boolean>();
+  const [showAdder, setShowAdder] = useState<boolean>(false);
   const onSubmit: FormEventHandler = (e) => {
     e.preventDefault();
     setSubmitted(true);
@@ -69,34 +72,52 @@ const TallyPage: FC = () => {
                   onTally={(t) => tallyIds.length == 1 && setTitleTally(t)}
                 />
               ))}
-              <div className="flex flex-col justify-between w-80 border-2 border-dashed border-gray-200 dark:border-gray-500 rounded-lg">
-                <div className="w-full text-center py-2 text-gray-400 dark:text-gray-500 font-medium">
-                  Add tally
-                </div>
-                <form className="flex flex-col p-12" onSubmit={onSubmit}>
-                  <TextInput
-                    type="text"
-                    minLength={minLength}
-                    maxLength={maxLength}
-                    className="text-center text-2xl"
-                    value={addtlId}
-                    onChange={(e) => setAddtlId(e.target.value)}
-                    showError={submitted && !isValid}
-                    errorMessage={validationMessage}
-                  />
+              {showAdder && (
+                <div className="flex flex-col relative justify-between w-80 border-2 border-dashed border-gray-200 dark:border-gray-500 rounded-lg">
                   <Button
-                    type="submit"
-                    size="lg"
-                    className="bg-emerald-500 p-2 m-4 text-white dark:text-black text-lg font-semibold rounded-md ring-1 ring-gray-900/5"
+                    onClick={() => setShowAdder(false)}
+                    size="flat"
+                    className="absolute -top-[2px] -right-[2px] text-white dark:text-black bg-gray-200 dark:bg-gray-500 rounded-tr-lg rounded-bl-lg p-1"
                   >
-                    Add
+                    <Icon path={mdiClose} size={1} />
                   </Button>
-                </form>
-              </div>
+                  <div className="w-full text-center py-2 text-gray-400 dark:text-gray-500 font-medium">
+                    Add tally
+                  </div>
+                  <form className="flex flex-col p-12" onSubmit={onSubmit}>
+                    <TextInput
+                      type="text"
+                      minLength={minLength}
+                      maxLength={maxLength}
+                      className="text-center text-2xl"
+                      value={addtlId}
+                      onChange={(e) => setAddtlId(e.target.value)}
+                      showError={submitted && !isValid}
+                      errorMessage={validationMessage}
+                    />
+                    <Button
+                      type="submit"
+                      size="lg"
+                      className="bg-emerald-500 p-2 m-4 text-white dark:text-black text-lg font-semibold rounded-md ring-1 ring-gray-900/5"
+                    >
+                      Add
+                    </Button>
+                  </form>
+                </div>
+              )}
             </div>
           )}
         </div>
       </Pusher>
+      {!showAdder && (
+        <Button
+          size="lg"
+          className="fixed bottom-0 right-0 bg-emerald-500 p-2 m-4 text-white dark:text-black text-lg font-semibold rounded-md ring-1 ring-gray-900/5"
+          onClick={() => setShowAdder(true)}
+        >
+          Add Tally
+        </Button>
+      )}
     </>
   );
 };
