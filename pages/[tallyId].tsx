444d8f26ea77051211a6dd361cd26e37ecc91b93
Martin Dennhardt <martin@dennhardt.de>
2022-03-29 07:32:56 +0000

    add input validation to "add tally" card

--- pages/[tallyId].tsx
+++ pages/[tallyId].tsx
@@ -1,16 +1,32 @@
-import React, { FC, useState } from "react";
+import React, { FC, FormEventHandler, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { Tally } from "hooks/useTally";
 import classNames from "classnames";
 import { Button, Pusher, TallyCard, TextInput } from "components";
-import { maxLength, minLength } from "modules/tally";
+import {
+  maxLength,
+  minLength,
+  validateId,
+  validationMessage,
+} from "modules/tally";
 
 const TallyPage: FC = () => {
   const router = useRouter();
   const tallyIds = (router.query.tallyId as string)?.split("+");
   const [titleTally, setTitleTally] = useState<Tally>();
-  const [addtlId, setAddtlId] = useState<string>();
+  const [addtlId, setAddtlId] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean>();
+  const [submitted, setSubmitted] = useState<boolean>();
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+    if (isValid) router.push(`${tallyIds.join("+")}+${addtlId}`);
+  };
+
+  useEffect(() => {
+    setIsValid(validateId(addtlId));
+  }, [addtlId]);
 
   return (
     <>
@@ -38,22 +54,23 @@ const TallyPage: FC = () => {
                 />
               ))}
               <div className="w-80 border-2 border-dashed rounded-lg flex justify-center">
-                <form className="flex flex-col justify-center">
+                <form
+                  className="flex flex-col justify-center"
+                  onSubmit={onSubmit}
+                >
                   <TextInput
                     type="text"
                     minLength={minLength}
                     maxLength={maxLength}
                     className="text-center"
                     onChange={(e) => setAddtlId(e.target.value)}
-                  ></TextInput>
+                    showError={submitted && !isValid}
+                    errorMessage={validationMessage}
+                  />
                   <Button
                     type="submit"
                     size="lg"
                     className="bg-emerald-500 p-2 m-4 text-white dark:text-black text-lg font-semibold rounded-md ring-1 ring-gray-900/5"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      router.push(`${tallyIds.join("+")}+${addtlId}`);
-                    }}
                   >
                     Add
                   </Button>
